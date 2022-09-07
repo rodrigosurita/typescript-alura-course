eea2afd388599df77785719c10a9870ce58d3dd8
Rodrigo <rodrigosurita@live.com>
2022-09-07 15:49:45 -0300

    filter added to negociacoes API response

--- app/dist/js/controllers/negociacao-controller.js
+++ app/dist/js/controllers/negociacao-controller.js
@@ -37,6 +37,13 @@ export class NegociacaoController {
     importarDados() {
         this.negociacoesService.obterNegociacoes()
             .then(negociacoesDeHoje => {
+            return negociacoesDeHoje.filter(negociacaoDeHoje => {
+                return !this.negociacoes
+                    .lista()
+                    .some(negociacao => negociacao.ehIgual(negociacaoDeHoje));
+            });
+        })
+            .then(negociacoesDeHoje => {
             for (let negociacao of negociacoesDeHoje) {
                 this.negociacoes.adiciona(negociacao);
             }
