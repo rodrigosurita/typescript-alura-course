c5d84e361c0c5615417faecdccd1a9e8d0ea6f95
Rodrigo <rodrigosurita@live.com>
2022-09-07 13:31:32 -0300

    decorator removed from the update method in view class

--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -14,8 +14,6 @@ export abstract class View<T> {
         }
     }
 
-    @inspect()
-    @logarTempoDeExecucao(true)
     public update(model: T): void {
         let template = this.template(model);
         this.elemento.innerHTML = template;
